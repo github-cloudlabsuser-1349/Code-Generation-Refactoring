41febaf5abbffefac2121ac5524cc7fea4f7d8d8
github_cloudlabsuser_1349 <github_cloudlabsuser_1349@cloudlabsaiuser.com>
2025-05-29 18:55:40 +0000

    mod1step7 - added user input and catching for invalid input

--- card_draw.py
+++ card_draw.py
@@ -1,17 +1,41 @@
-# Fixed Python program to draw five random cards
+# Fixed Python program to draw a specified number of random cards
 
 import itertools
 import random
 
-# Create a deck of cards with ranks and suits
-ranks = [str(n) for n in range(2, 11)] + ['Jack', 'Queen', 'King', 'Ace']
-suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
-deck = list(itertools.product(ranks, suits))
+def get_num_cards():
+    while True:
+        try:
+            num = int(input("How many cards would you like to draw? (1-52): "))
+            if 1 <= num <= 52:
+                return num
+            else:
+                print("Please enter a number between 1 and 52.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
 
-# Shuffle the deck
-random.shuffle(deck)
+def main():
+    # Create a deck of cards with ranks and suits
+    ranks = [str(n) for n in range(2, 11)] + ['Jack', 'Queen', 'King', 'Ace']
+    suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+    deck = list(itertools.product(ranks, suits))
 
-# Draw five cards
-print("You got:")
-for rank, suit in deck[:5]:
-    print(f"{rank} of {suit}")
+    # Shuffle the deck
+    random.shuffle(deck)
+
+    num_cards = get_num_cards()
+
+    # Draw cards
+    print("You got:")
+    for rank, suit in deck[:num_cards]:
+        print(f"{rank} of {suit}")
+
+    # Handle scenario: drawing all cards
+    if num_cards == 52:
+        print("You have drawn the entire deck!")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nProgram terminated by user.")
